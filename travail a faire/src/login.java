50e369a0fbc47a64d76719b0d76398cf1c8cbe29
nessrinemezni <127732456+nessrinemezni@users.noreply.github.com>
2023-04-16 17:38:53 +0200

    Add files via upload

---
+++ travail a faire/src/login.java
@@ -0,0 +1,57 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter printWriter=response.getWriter();
+		String userName=request.getParameter("userName");
+		String password=request.getParameter("password");
+		if(password.equals("nessrine")) {
+			RequestDispatcher requestDispatcher=request.getRequestDispatcher("userProfiles");
+			
+			requestDispatcher.forward(request,response);
+			}else {
+			printWriter.println("please check User/password");
+			RequestDispatcher requestDispatcher=request.getRequestDispatcher("/index.html");
+			requestDispatcher.include(request, response);} 
+			
+		}
+	}
+
+
---
+++ travail a faire/src/userProfiles.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class userProfiles
+ */
+@WebServlet("/userProfiles")
+public class userProfiles extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public userProfiles() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	response.setContentType("text/html");
+	PrintWriter printWriter=response.getWriter();
+	String userName= request.getParameter("userName");
+	printWriter.println("welcome in userProfile area");
+	printWriter.println("welcome:"+userName);
+		
+	}
+
+}
